c59f821560a87792aeeb8264201ef357500e2d9f
Tomás Couso <tcouso@uc.cl>
2024-03-09 12:30:15 -0300

    Fix: robustness agains network failure

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -6,26 +6,37 @@ from src.notifications import send_telegram_notification
 
 
 def main():
-    logger = get_logger()
+    try:
+        logger = get_logger()
 
-    message = "Starting signal simulations"
-    logger.info(message)
-    send_telegram_notification(message)
-    run_simulations()
+        message = "Starting signal simulations"
+        logger.info(message)
+        send_telegram_notification(message)
+        run_simulations()
 
-    message = "Starting pixel predictions"
-    logger.info(message)
-    send_telegram_notification(message)
-    predict_over_pixels()
+        message = "Starting pixel predictions"
+        logger.info(message)
+        send_telegram_notification(message)
+        predict_over_pixels()
 
-    message = "Starting polygon predictions"
-    logger.info(message)
-    send_telegram_notification(message)
-    predict_over_polygons()
+        message = "Starting polygon predictions"
+        logger.info(message)
+        send_telegram_notification(message)
+        predict_over_polygons()
 
-    message = "Done"
-    logger.info(message)
-    send_telegram_notification(message)
+        message = "Done"
+        logger.info(message)
+        send_telegram_notification(message)
+
+    except KeyboardInterrupt:
+        message = "Execution interrupted by keyboard"
+        logger.info(message)
+        send_telegram_notification(message)
+
+    except Exception as e:
+        message = f"Execution crashed: {e}"
+        logger.error(message)
+        send_telegram_notification(message)
 
 
 if __name__ == '__main__':
--- src/notifications.py
+++ src/notifications.py
@@ -1,6 +1,8 @@
 import requests
 import os
 from dotenv import find_dotenv, load_dotenv
+from src.config import get_logger
+import time
 
 load_dotenv(find_dotenv())
 
@@ -8,9 +10,18 @@ TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
 API_URL = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}"
 
+logger = get_logger()
+
+
 def send_telegram_notification(message):
     url = API_URL + "/sendMessage"
     payload = {"chat_id": CHAT_ID, "text": message}
 
-    response = requests.post(url, data=payload)
-    return response.json()
+    try:
+
+        response = requests.post(url, data=payload)
+        response.raise_for_status()
+
+    except requests.RequestException:
+
+        logger.info("Failed to send Telegram notification:")
